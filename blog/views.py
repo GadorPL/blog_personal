13adeb973a8963c1fe5689d0a68b22962271c472
Michał Hewelt <mhewelt9@gmail.com>
2022-06-15 14:32:37 +0200

    add temporary data

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,85 @@
+from datetime import date
 from django.shortcuts import render
+
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Michał",
+        "date": date(2022, 6, 15),
+        "title": "Mountain Hiking",
+        "excerpt": "There's noting like the views you get when hiking in the mountains! "
+                  "And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.   
+            
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.
+            
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.
+        """,
+    },
+
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Michał",
+        "date": date(2022, 6, 14),
+        "title": "Programming is Fun",
+        "excerpt": "There's noting like the views you get when hiking in the mountains! "
+                   "And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.   
+
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.
+
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.
+        """,
+    },
+
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Michał",
+        "date": date(2022, 6, 13),
+        "title": "Nature At Its Best",
+        "excerpt": "There's noting like the views you get when hiking in the mountains! "
+                   "And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.   
+
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.
+
+            Pol, a bene itineris tramitem, coordinatae! Make it so, alignment!
+            Our shining harmony for heaven is to capture others extraordinarily.
+            You have to listen, and feel trust by your luring.
+            Jolly! Pieces o' pestilence are forever proud.
+        """,
+    }
+]
 
 
 # Create your views here.
