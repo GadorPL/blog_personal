e3592c1fc1796ea923f12316a79f8dc24eba312c
Michał Hewelt <mhewelt9@gmail.com>
2022-11-21 10:32:08 +0100

    fix 'read later' bug

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4dg=n!g5k-5hrw4)a(#!2_nqo8umu4j()*u&y-ijvp0zlf=l83'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
