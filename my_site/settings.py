9e794d85097d2c0ef1339f3d10257327db06deb9
Michał Hewelt <mhewelt9@gmail.com>
2022-11-21 22:33:08 +0100

    ready for deployment, add postgresql

--- my_site/settings.py
+++ my_site/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from os import getenv
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': getenv('DBUSER'),
+        'PASSWORD': getenv('DBPASSWORD'),
+        'HOST': getenv('DBHOST'),
+        'PORT': '5432',
     }
 }
 
